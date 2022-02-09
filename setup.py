43737b07c785f5f3f5a9de016cff9c989eafadee
Cesar Carrillo <crcg1995@hotmail.com>
2022-02-09 02:06:13 -0600

    Added requirements and setup for installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='TestInstruments',
+   version='0.01',
+   description='A useful module to simulate behavior of lab instruments',
+   author='Cesar Carrillo',
+   author_email='crcg1995@hotmail.com',
+   packages=['Generators','Indicators'],  #same as name
+   install_requires=['numpy'], #external packages as dependencies
+)
