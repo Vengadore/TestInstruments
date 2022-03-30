f3d5c8266ba7cfb89cd586ef2741744e18145bcb
Mony Medina :) <80802175+MonicaS97@users.noreply.github.com>
2022-03-30 15:29:04 -0600

    Delete traductor.py

--- Indicators/traductor.py
+++
@@ -1,7 +0,0 @@
-
-    ## Traducción de Prefijos
-    def traductor(valor):
-        print(valor.split(" "))
-        val,pref = valor.split(" ")
-        val = int(val) * Prefijos[pref]
-        return val
