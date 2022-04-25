01cf242a9fd4300713a69a5a461af11233175b7e
Monica <monysilva049@gmail.com>
2022-04-25 09:51:12 -0500

    Agregar clase 5500A

--- Generators/cal5XXXA.py
+++ Generators/cal5XXXA.py
@@ -116,9 +116,23 @@ class FLUKE_5500A:
 
     def __init__(self, bus_connection:str = "GPIB0",simulation = True):
         self.name = "FLUKE 5500A"
+        self.bus = bus_connection
+        if not self.SIM:
+            self.init_visa_connection()
+
+    def init_visa_connection(self):
+        rm = pyvisa.ResourceManager()
+        self.inst = rm.open_resource(self.bus)
+        return 1
+
+    def send_visa_cmd(self,cmd):
+        self.inst.write(cmd)
+        self.inst.write("*WAI")
+        return 1
 
     compensation = {0:"None", 2:"WIRE2", 4:"WIRE4"} 
-    def convertion(valor): 
+    
+    def convertion(self,valor): 
         Prefijos = {"K":1E3,"M":1E6,"G":1E9,"m":1E-3,"uA":1E-6,"nA":1E-9,"V":1E0,"A":1E0,"OHM":1E0, "mV":1E-3, "mA":1E-3, "mF":1E-3, "uF":1E-6, "nF":1E-9, "pF":1E-12}
         if isinstance(valor, str):
             print(valor.split(" "))
@@ -132,13 +146,13 @@ class FLUKE_5500A:
     def set_DCV(self,amplitud_ch1,amplitud_ch2 = None):
         cha1 = self.convertion(amplitud_ch1)
         if amplitud_ch2 == None:
-            self.send_visa_cmd(f"OUT {cha1} V")
+            self.send_visa_cmd(f"OUT {cha1} V, 0 HZ")
         else:
             cha2 = self.convertion(amplitud_ch2)
-            self.send_visa_cmd(f"OUT {cha1} V, {cha2} V")   
+            self.send_visa_cmd(f"OUT {cha1} V, {cha2} V, 0 HZ")   
         return 0 
 
-    def set_ACV(self,amplitud_ch1,frequencia,amplitud_ch2 = None)
+    def set_ACV(self,amplitud_ch1,frequencia,amplitud_ch2 = None):
         cha1 = self.convertion(amplitud_ch1)
         if amplitud_ch2 == None:
             self.send_visa_cmd(f"OUT {cha1} V, {frequencia} HZ")
@@ -149,7 +163,7 @@ class FLUKE_5500A:
 
     def set_DCI(self,amplitud):
         amp = self.convertion(amplitud)
-        self.send_visa_cmd(f"OUT {amp} A")   
+        self.send_visa_cmd(f"OUT {amp} A, 0 HZ")   
         return 0
 
     def set_ACI(self,amplitud,frequencia):
@@ -167,6 +181,7 @@ class FLUKE_5500A:
             self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         else:
             self.send_visa_cmd(f"OUT {amp} OHM")
+            self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         return 0 
 
     def set_CAP(self,amplitud,compensation = 0):
@@ -174,11 +189,9 @@ class FLUKE_5500A:
         if compensation == 2:
             self.send_visa_cmd(f"OUT {amp} F")
             self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
-        elif compensation == 4:
-            self.send_visa_cmd(f"OUT {amp} F")
-            self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         else:
             self.send_visa_cmd(f"OUT {amp} F")
+            self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         return 0 
 
     def set_RTD(self,amplitud,tipo = "PT385", compensation = 0):
@@ -192,6 +205,7 @@ class FLUKE_5500A:
             self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         else:
             self.send_visa_cmd(f"OUT {amplitud} CEL")
+            self.send_visa_cmd(f"ZCOMP {self.compensation[compensation]}")
         return 0 
 
     def set_TC(self,amplitud,tipo):
@@ -204,8 +218,17 @@ class FLUKE_5500A:
         self.send_visa_cmd(f"TC_MEAS")
         self.send_visa_cmd(f"TC_TYPE {tipo}")
 
-    
+    def set_POWER_DC(self,amplitud_VCC,amplitud_ICC):
+         aVCC= self.convertion(amplitud_VCC)
+         aICC= self.convertion(amplitud_ICC)
+         self.send_visa_cmd(f"OUT {aVCC} V,{aICC} A")
+         return 0
 
+    def set_POWER_AC(self,amplitud_VCA,amplitud_ICA,frequencia):
+         aVCA= self.convertion(amplitud_VCA)
+         aICA= self.convertion(amplitud_ICA)
+         self.send_visa_cmd(f"OUT {aVCA} V,{aICA} A,{frequencia} HZ")
+         return 0
 
 if __name__ == "__main__":
     Instrument = Fluke_5720A()
