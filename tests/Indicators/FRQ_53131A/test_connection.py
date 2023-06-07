da5551edc2b944bd382420a86ca5e628771b3827
Cesar Carrillo <crcg1995@hotmail.com>
2023-06-07 09:03:22 -0600

    Esqueleto inicial

---
+++ tests/Indicators/FRQ_53131A/test_connection.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+## Seleccionar un instrumento a travez de GPIB y mandar un comando
+## simple de identificación, el instrumento debe responder con la
+## información: HEWLETT-PACKARD, 53131A,0,XXXX
+## El comando a mandar es *IDN?
---
+++ tests/Indicators/FRQ_53131A/test_reset.py
@@ -0,0 +1,6 @@
+import unittest
+
+## Una vez establecida la conexión, esperar 5 segundos, mandar
+## comando de reset y después de 4 segundos pedir el ID.
+## El comando es *RST
+
