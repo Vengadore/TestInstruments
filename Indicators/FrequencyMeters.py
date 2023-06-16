929b6c2110e0ec2bc183988bab95fbc2b8f08880
Monica <monysilva049@gmail.com>
2023-06-16 10:39:11 -0600

    Se agrega configuracion y Tests

--- Indicators/FrequencyMeters.py
+++ Indicators/FrequencyMeters.py
@@ -14,7 +14,7 @@ class Counter_53131A:
     def init_visa_connection(self):
         rm = pyvisa.ResourceManager()
         self.inst = rm.open_resource(self.bus, read_termination = "\r")
-        self.inst.timeout = 50000
+        self.inst.timeout = 5000
         return 0
     
     def reset(self):
@@ -29,3 +29,37 @@ class Counter_53131A:
     
     def get_ERROR(self):
         return self.inst.query(":SYST:ERR?")
+    
+    def write(self,command):
+        self.inst.write(command)
+
+    def set_LPF(self,enable = True):
+        if enable == True:
+            self.write(":INP1:FILT ON")
+        else:
+            self.write(":INP1:FILT OFF")
+    
+    def set_Level(self,nivel_tension):
+        if nivel_tension >= -5.125 or nivel_tension <= 5.125:
+            self.write(f":SENS:EVEN1:LEV:ABS {nivel_tension}")
+
+    def set_SLOPE(self,polaridad):
+        self.write(f":SENS:EVEN1:SLOP {polaridad}")
+
+    def set_SENSTVTY(self,sensibilidad):
+        if sensibilidad == "HI" :
+            self.write(f":SENS:EVEN1:HYST:REL 0")
+        elif sensibilidad == "MED" :
+            self.write(f":SENS:EVEN1:HYST:REL 50")
+        elif sensibilidad == "LO" :
+            self.write(f":SENS:EVEN1:HYST:REL 100")
+
+    def set_COUPLING(self,tipo_AC_DC):
+            self.write(f":INP1:COUP {tipo_AC_DC}")
+
+    def set_IMPEDANCE(self,valor):
+            if valor == '50 OHM':
+               self.write(":INP1:IMP 50") 
+            elif valor == '1 MOHM':
+                self.write(":INP1:IMP 1E6") 
+
--- tests/Indicators/FRQ_53131A/test_configuration.py
+++ tests/Indicators/FRQ_53131A/test_configuration.py
@@ -5,6 +5,7 @@ from FrequencyMeters import Counter_53131A
 
     ## Available tests
 #   FILTER
+#   LEVEL
 #   AUTO TRG   
 #   SLOPE
 #   SENSITIVITY 
@@ -13,6 +14,51 @@ from FrequencyMeters import Counter_53131A
 class ConfigurationTest(unittest.TestCase):
     def setup(self):
         self.Instrument = Counter_53131A(bus_connection='GPIB0::3::INSTR')
+
+    def close_conection(self):
+        self.Instrument.disconnect()
+
+    def test_configure_Filter(self):
+        self.setup()
+        self.Instrument.set_LPF()
+        estado = self.Instrument.inst.query(":INP1:FILT:LPAS:STAT?")
+        self.assertCountEqual(estado,"1")
+        self.close_conection()
+
+    def test_configure_Level(self):
+        self.setup()
+        self.Instrument.set_Level(2)
+        tension = self.Instrument.inst.query(":SENS:EVEN1:LEV:ABS?")
+        self.assertEqual(float(tension),2.0)
+        self.close_conection()
+    
+    def test_configure_SLOPE(self):
+        self.setup()
+        self.Instrument.set_SLOPE("POS")
+        pol = self.Instrument.inst.query(":SENS:EVEN1:SLOP?")
+        self.assertEqual(pol,"POS")
+        self.close_conection()
+
+    def test_configure_SENSTVTY(self):
+        self.setup()
+        self.Instrument.set_SENSTVTY("LO")
+        sens = self.Instrument.inst.query(":SENS:EVEN1:HYST:REL?")
+        self.assertEqual(sens,'+100')
+        self.close_conection()
+
+    def test_configure_COUPLING(self):
+        self.setup()
+        self.Instrument.set_COUPLING("DC")
+        coup = self.Instrument.inst.query(":INP:COUP?")
+        self.assertEqual(coup,"DC")
+        self.close_conection()
+    
+    def test_configure_IMPEDANCE(self):
+        self.setup()
+        self.Instrument.set_IMPEDANCE('50 OHM')
+        imp = self.Instrument.inst.query(":INP1:IMP?")
+        self.assertEqual(float(imp),50.0)
+        self.close_conection()
 
     
 
