7248eded1bc8860456b0e8fa7016fd62f05acaf4
Monica <monysilva049@gmail.com>
2023-06-21 17:31:56 -0600

    Se agregan nuevos TESTS

--- tests/Indicators/FRQ_53131A/test_configuration.py
+++ tests/Indicators/FRQ_53131A/test_configuration.py
@@ -18,7 +18,7 @@ class ConfigurationTest(unittest.TestCase):
     def close_conection(self):
         self.Instrument.disconnect()
 
-    def test_configure_Filter(self):
+    def test_configure_Filter_ON(self):
         self.setup()
         self.Instrument.set_LPF()
         estado = self.Instrument.inst.query(":INP1:FILT:LPAS:STAT?")
@@ -32,28 +32,28 @@ class ConfigurationTest(unittest.TestCase):
         self.assertEqual(float(tension),2.0)
         self.close_conection()
     
-    def test_configure_SLOPE(self):
+    def test_configure_SLOPE_POS(self):
         self.setup()
         self.Instrument.set_SLOPE("POS")
         pol = self.Instrument.inst.query(":SENS:EVEN1:SLOP?")
         self.assertEqual(pol,"POS")
         self.close_conection()
 
-    def test_configure_SENSTVTY(self):
+    def test_configure_SENSTVTY_LO(self):
         self.setup()
         self.Instrument.set_SENSTVTY("LO")
         sens = self.Instrument.inst.query(":SENS:EVEN1:HYST:REL?")
         self.assertEqual(sens,'+100')
         self.close_conection()
 
-    def test_configure_COUPLING(self):
+    def test_configure_COUPLING_DC(self):
         self.setup()
         self.Instrument.set_COUPLING("DC")
         coup = self.Instrument.inst.query(":INP:COUP?")
         self.assertEqual(coup,"DC")
         self.close_conection()
     
-    def test_configure_IMPEDANCE(self):
+    def test_configure_IMPEDANCE_50OHM(self):
         self.setup()
         self.Instrument.set_IMPEDANCE('50 OHM')
         imp = self.Instrument.inst.query(":INP1:IMP?")
@@ -66,6 +66,49 @@ class ConfigurationTest(unittest.TestCase):
         tension = self.Instrument.inst.query(":FREQ:ARM:STOP:TIM?")
         self.assertEqual(float(tension),1.0)
         self.close_conection()
+
+    def test_configure_Filter_OFF(self):
+        self.setup()
+        self.Instrument.set_LPF(False)
+        estado = self.Instrument.inst.query(":INP1:FILT:LPAS:STAT?")
+        self.assertCountEqual(estado,"0")
+        self.close_conection()
+
+    def test_configure_SLOPE_NEG(self):
+        self.setup()
+        self.Instrument.set_SLOPE("NEG")
+        pol = self.Instrument.inst.query(":SENS:EVEN1:SLOP?")
+        self.assertEqual(pol,"NEG")
+        self.close_conection()
+
+    def test_configure_SENSTVTY_MED(self):
+        self.setup()
+        self.Instrument.set_SENSTVTY("MED")
+        sens = self.Instrument.inst.query(":SENS:EVEN1:HYST:REL?")
+        self.assertEqual(sens,'+50')
+        self.close_conection()
+
+    def test_configure_COUPLING_AC(self):
+        self.setup()
+        self.Instrument.set_COUPLING("AC")
+        coup = self.Instrument.inst.query(":INP:COUP?")
+        self.assertEqual(coup,"AC")
+        self.close_conection()
+
+    def test_configure_IMPEDANCE_1MOHM(self):
+        self.setup()
+        self.Instrument.set_IMPEDANCE('1 MOHM')
+        imp = self.Instrument.inst.query(":INP1:IMP?")
+        self.assertEqual(float(imp),1000000.0)
+        self.close_conection()
+
+    def test_configure_SENSTVTY_HI(self):
+        self.setup()
+        self.Instrument.set_SENSTVTY("HI")
+        sens = self.Instrument.inst.query(":SENS:EVEN1:HYST:REL?")
+        self.assertEqual(sens,'+0')
+        self.close_conection()
+
 
     
 
