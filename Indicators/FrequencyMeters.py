817a4a0ee89fbd7b1cb14ea7d6f417ed1e3b4b19
Vengadore <Vengadore@users.noreply.github.com>
2023-06-28 08:41:34 -0600

    Conexión del counter con el GPIB (#21)
    * Conexión del counter con el GPIB
    
    * Test de reset del instrumento
    
    * Fix de test
    
    * Test Connection and Reset Completed
    
    * Test error Completed
    
    * Se agrega configuracion y Tests
    
    * Se agrega configuracion y Test Gate time
    
    * Se agregan nuevos TESTS
    
    * Se aplican correciones
    
    * Se agregan configuraciones
    
    ---------
    
    Co-authored-by: Monica <monysilva049@gmail.com>

---
+++ Indicators/FrequencyMeters.py
@@ -0,0 +1,75 @@
+import pyvisa
+import numpy as np
+import time
+from constants import *
+
+class Counter_53131A:
+    def __init__(self, bus_connection:str = "GPIB0"):
+        self.name = "Universal Counter 53131A"
+        self.bus = bus_connection
+        self.init_visa_connection()
+    
+    def __name__(self):
+        return "indicator"
+
+    def init_visa_connection(self):
+        rm = pyvisa.ResourceManager()
+        self.inst = rm.open_resource(self.bus, read_termination = "\r")
+        self.inst.timeout = 5000
+        return 0
+    
+    def reset(self):
+        self.inst.write("*RST")
+        return 0
+    
+    def disconnect(self):
+        self.inst.close()
+
+    def get_IDN(self):
+        return self.inst.query("*IDN?")
+    
+    def get_ERROR(self):
+        return self.inst.query(":SYST:ERR?")
+    
+    def write(self,command):
+        self.inst.write(command)
+
+    def set_LPF(self,enable = True):
+        if enable == True:
+            self.write(":INP1:FILT ON")
+        else:
+            self.write(":INP1:FILT OFF")
+    
+    def set_Level(self,nivel_tension):
+        if nivel_tension >= -5.125 or nivel_tension <= 5.125:
+            self.write(f":SENS:EVEN1:LEV:ABS {nivel_tension}")
+
+    def set_SLOPE(self,polaridad):
+        self.write(f":SENS:EVEN1:SLOP {polaridad}")
+
+    def set_SENSTVTY(self,sensibilidad = "MED"):
+        if sensibilidad == "HI" :
+            self.write(f":SENS:EVEN1:HYST:REL 0")
+        elif sensibilidad == "MED" :
+            self.write(f":SENS:EVEN1:HYST:REL 50")
+        elif sensibilidad == "LO" :
+            self.write(f":SENS:EVEN1:HYST:REL 100")
+
+    def set_COUPLING(self,tipo_AC_DC = "AC"):
+            self.write(f":INP1:COUP {tipo_AC_DC}")
+
+    def set_IMPEDANCE(self,valor = "1 MOHM"):
+            if valor == '50 OHM':
+               self.write(":INP1:IMP 50") 
+            elif valor == '1 MOHM':
+                self.write(":INP1:IMP 1E6") 
+    
+    def set_Gate_Time(self,time = 5.000):
+        self.write(f":FREQ:ARM:STOP:TIM {time}")
+
+    def read(self):
+        return self.inst.query("READ?")
+        
+    def RUN(self):
+        self.write(":INIT:CONT ON")
+
--- tests/Indicators/FRQ_53131A/test_reset.py
+++ tests/Indicators/FRQ_53131A/test_reset.py
@@ -1,6 +1,23 @@
 import unittest
+import sys
+sys.path.append("./Indicators")
+from FrequencyMeters import Counter_53131A 
 
 ## Una vez establecida la conexión, esperar 5 segundos, mandar
 ## comando de reset y después de 4 segundos pedir el ID.
 ## El comando es *RST
 
+class ResetTest(unittest.TestCase):
+    def setup(self):
+        self.Instrument = Counter_53131A(bus_connection='GPIB0::3::INSTR')
+
+    def test_reset(self):
+        self.setup()
+        self.Instrument.reset()
+        self.close_conection()
+
+    def close_conection(self):
+        self.Instrument.disconnect()
+
+if __name__ == '__main__':
+    unittest.main()
